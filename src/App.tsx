919b83057693caf27651f1a831b1e8beb9babd9a
bhargavkhatri <bhargavkhatri786@gmail.com>
2026-08-01 18:54:57 +0530

    chore: establish project architecture

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-300">
+      <h1 className="text-2xl font-bold">Employee Management System</h1>
+    </div>
+  )
+}
+
+export default App
